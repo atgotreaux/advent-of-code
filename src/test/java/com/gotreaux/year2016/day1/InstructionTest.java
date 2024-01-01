809e2d7ba83ef813a21a4f10bd72f7dbfc2ef0e6
atgotreaux <atgotreaux@users.noreply.github.com>
2023-12-31 18:53:34 -0600

    2016 Day 1

---
+++ src/test/java/com/gotreaux/year2016/day1/InstructionTest.java
@@ -0,0 +1,25 @@
+package com.gotreaux.year2016.day1;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class InstructionTest {
+    @Test
+    void parseRight() {
+        assertEquals(Instruction.RIGHT, Instruction.fromLabel('R'));
+    }
+
+    @Test
+    void parseLeft() {
+        assertEquals(Instruction.LEFT, Instruction.fromLabel('L'));
+    }
+
+    @Test
+    void throwsIfCannotParse() {
+        assertThrows(NoSuchElementException.class, () -> Instruction.fromLabel('X'));
+    }
+}
