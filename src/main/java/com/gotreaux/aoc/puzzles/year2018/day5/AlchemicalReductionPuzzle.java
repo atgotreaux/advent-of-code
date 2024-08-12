e684af2dfc0874a37643bdb5ac6eceafd967fa46
atgotreaux <atgotreaux@users.noreply.github.com>
2024-08-11 23:28:21 -0500

    2018 Day 5

---
+++ src/main/java/com/gotreaux/aoc/puzzles/year2018/day5/AlchemicalReductionPuzzle.java
@@ -0,0 +1,60 @@
+package com.gotreaux.aoc.puzzles.year2018.day5;
+
+import com.gotreaux.aoc.annotations.ShellPuzzle;
+import com.gotreaux.aoc.input.InputProvider;
+import com.gotreaux.aoc.output.PuzzleOutput;
+import com.gotreaux.aoc.puzzles.Puzzle;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@ShellPuzzle(year = 2018, day = 5, title = "Alchemical Reduction")
+public class AlchemicalReductionPuzzle extends Puzzle {
+    public AlchemicalReductionPuzzle(InputProvider inputProvider) {
+        super(inputProvider);
+    }
+
+    @Override
+    public PuzzleOutput<Integer, Integer> solve() throws IOException, URISyntaxException {
+        String input = getInputProvider().getInputString();
+
+        String collapsedPolymer = collapse(input);
+
+        int shortestCollapsedPolymer =
+                input.chars()
+                        .map(Character::toLowerCase)
+                        .distinct()
+                        .mapToObj(
+                                i ->
+                                        input.replace(Character.toString(i), "")
+                                                .replace(
+                                                        Character.toString(
+                                                                Character.toUpperCase(i)),
+                                                        ""))
+                        .map(AlchemicalReductionPuzzle::collapse)
+                        .map(String::length)
+                        .min(Integer::compareTo)
+                        .orElseThrow();
+
+        return new PuzzleOutput<>(collapsedPolymer.length(), shortestCollapsedPolymer);
+    }
+
+    private static String collapse(String input) {
+        StringBuilder result = new StringBuilder(input.length());
+        for (int i = 0; i < input.length(); i++) {
+            int x = input.codePointAt(i);
+            if (i < input.length() - 1
+                    && x != input.codePointAt(i + 1)
+                    && Character.toLowerCase(x)
+                            == Character.toLowerCase(input.codePointAt(i + 1))) {
+                i++;
+            } else {
+                result.appendCodePoint(x);
+            }
+        }
+
+        if (result.length() == input.length()) {
+            return input;
+        }
+        return collapse(result.toString());
+    }
+}
---
+++ src/test/java/com/gotreaux/aoc/puzzles/year2018/day5/AlchemicalReductionPuzzleTest.java
@@ -0,0 +1,46 @@
+package com.gotreaux.aoc.puzzles.year2018.day5;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.gotreaux.aoc.input.InputProvider;
+import com.gotreaux.aoc.input.StringInputProvider;
+import com.gotreaux.aoc.output.PuzzleOutput;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class AlchemicalReductionPuzzleTest {
+    @ParameterizedTest
+    @MethodSource("provideCollapsedPolymer")
+    void collapsedPolymer(String input, int expectedUnits) throws Exception {
+        InputProvider inputProvider = new StringInputProvider(input);
+
+        AlchemicalReductionPuzzle puzzle = new AlchemicalReductionPuzzle(inputProvider);
+
+        PuzzleOutput<Integer, Integer> output = puzzle.solve();
+
+        assertEquals(expectedUnits, output.partOne());
+    }
+
+    @Test
+    void shortestCollapsedPolymer() throws Exception {
+        InputProvider inputProvider = new StringInputProvider("dabAcCaCBAcCcaDA");
+
+        AlchemicalReductionPuzzle puzzle = new AlchemicalReductionPuzzle(inputProvider);
+
+        PuzzleOutput<Integer, Integer> output = puzzle.solve();
+
+        assertEquals(4, output.partTwo());
+    }
+
+    private static Stream<Arguments> provideCollapsedPolymer() {
+        return Stream.of(
+                Arguments.of("aA", 0),
+                Arguments.of("abBA", 0),
+                Arguments.of("abAB", 4),
+                Arguments.of("aabAAB", 6),
+                Arguments.of("dabAcCaCBAcCcaDA", 10));
+    }
+}
